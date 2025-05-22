9641be1ac5fe551f396cb2fbaa2b06e64bf6c3b3
Maurits <maurits@Mauritss-Laptop.local>
2025-05-22 22:48:59 +0200

    Career page

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
 // Routes
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/careers', (req, res) => {
+  res.render('careers');
+});
+
+// Export the app for Vercel
 module.exports = app;
 
 // Start server locally (only when not running on Vercel)
